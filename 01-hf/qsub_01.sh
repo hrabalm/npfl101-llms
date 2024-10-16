838961949ba814c39cd025da21683cee0dfc0716
Miroslav Hrabal <mhn@email.cz>
2024-10-17 01:47:43 +0200

    wip

--- 01-hf/qsub_01.sh
+++ 01-hf/qsub_01.sh
@@ -3,7 +3,8 @@
 #PBS -q gpu
 #PBS -l select=1:ncpus=1:mem=16gb:scratch_local=16gb:ngpus=1:gpu_cap=compute_70:gpu_mem=15gb
 #PBS -l walltime=00:30:00
-#PBS -m bae  # mail on begin, abort, end
+#PBS -m bae
+# mail on begin, abort, end
 
 echo ${PBS_O_LOGNAME:?This script must be run under PBS scheduling system, execute: qsub $0}
 

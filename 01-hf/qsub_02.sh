eeceaa1a06e13511258f6f6c3b67c1820bbf47e9
Miroslav Hrabal <mhn@email.cz>
2024-10-17 03:12:25 +0200

    wip

--- 01-hf/qsub_02.sh
+++ 01-hf/qsub_02.sh
@@ -9,6 +9,7 @@
 source "$PBS_O_WORKDIR/common.sh"
 
 FILE="$SCRIPT_DIR/02_train.py"
+cd "$SCRIPT_DIR"  # note that in some cases this can affect performance, depending on how the cwd is used
 "$PYTHON" "$FILE"
 
 clean_scratch

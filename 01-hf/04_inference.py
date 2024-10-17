74d9ea8ff465690c8997248149362bbdc5889036
Miroslav Hrabal <mhn@email.cz>
2024-10-17 03:33:51 +0200

    wip

--- 01-hf/04_inference.py
+++
@@ -1,5 +0,0 @@
-# see also https://huggingface.co/docs/transformers/llm_tutorial
-# and https://huggingface.co/docs/transformers/llm_tutorial_optimization
-
-# you might also consider using other inference libraries, such as vllm or text-generation-inference
-
